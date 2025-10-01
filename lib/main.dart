752a58c7b1ad3f80e854ba4f1e3e1764b78660a8
TOUGMA07 <tougma07@gmail.com>
2025-10-01 16:38:04 +0000

    Mise à jour de main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,23 @@
-// Fichier principal de l'application
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/main_app.dart'; // Changement ici
 
-// Fonction principale - point d'entrée de l'app
 void main() {
   runApp(const MyApp());
 }
 
-// Classe principale de l'application
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // Méthode build pour l'application entière
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Désactive la bannière de debug
       debugShowCheckedModeBanner: false,
-      
-      // Titre de l'application
       title: 'Santé Market',
-      
-      // Thème de l'application
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      
-      // Premier écran à afficher
-      home: const HomeScreen(),
+      home: const MainApp(), // Changement ici
     );
   }
 }
-// main() : Point de départ de toute app Dart
-
-// runApp() : Lance l'application Flutter
-
-// MaterialApp : Application avec design Material (style Google)
